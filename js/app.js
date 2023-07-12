637bca61109facd26aa61dff5d36126dec881dd6
GTtron816 <abinak322@gmail.com>
2023-07-12 14:35:48 +0530

    anonymous functions

--- js/app.js
+++ js/app.js
@@ -1,18 +1,14 @@
 var list=[];
-function add(){
+const a=function(){
 if(!document.getElementById("tb").value == ""){
 list.push(document.getElementById("tb").value);
 document.getElementById("tb").value = "";
-print();
+p();
 }
 }
 
-function print(){
+const p=()=>{
     document.getElementById("taskhead").hidden=false;
     document.getElementById("tasks").innerHTML+="<input class='chk' type='checkbox'> <label>"+list[0]+"</label><br>";
     list.pop();
-
-
-
-
 }
