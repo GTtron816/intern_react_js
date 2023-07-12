e1ba32c3e6e722525d102c6fd22aa1782e94e188
GTtron816 <abinak322@gmail.com>
2023-07-12 17:48:44 +0530

    tasks

--- js/app.js
+++ js/app.js
@@ -9,6 +9,34 @@ p();
 
 const p=()=>{
     document.getElementById("taskhead").hidden=false;
-    document.getElementById("tasks").innerHTML+="<input class='chk' type='checkbox'> <label>"+list[0]+"</label><br>";
+    var chk=document.createElement("input")
+    chk.type="checkbox"
+    chk.id="chk";
+    chk.className="chk"
+    var span=document.createElement("span");
+    span.innerHTML=list[0];
+    span.id="span";
+    span.className="label";
+    var btn=document.createElement("button");
+    btn.textContent="Remove";
+    btn.className="btr";
+    var space=document.createElement("br");
+    chk.onclick=function(){
+        span.style.setProperty("text-decoration", "line-through");
+    };
+    btn.onclick=function(){
+        chk.remove();
+        span.remove();
+        btn.remove();
+        space.remove();
+
+    };
+
+    var parent=document.getElementById("tasks");
+    parent.appendChild(chk);
+    parent.appendChild(span);
+    parent.appendChild(btn);
+    parent.appendChild(space);
     list.pop();
 }
+
