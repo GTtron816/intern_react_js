7650718f5775e5fb28b5c85e0456d9c3e71e3a4f
GTtron816 <abinak322@gmail.com>
2023-07-15 11:14:55 +0530

    changes

---
+++ react/my-app/src/components/login.js
@@ -0,0 +1,16 @@
+function Loginform(){
+    return(
+        <div className="Login">
+        <form>
+  
+         <label>user name</label>
+         <input type="text"></input>
+         <label>password</label>
+         <input type="password"></input>
+         <input type="submit"></input>
+        </form>
+  
+      </div>
+    )
+}
+export {Loginform};
